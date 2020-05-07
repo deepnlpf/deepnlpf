63b2019fc85f0454536bb133a7eb8b8a0c599bc8
rodriguesfas <franciscosouzaacer@gmail.com>
2020-05-07 04:47:50 -0300

    update.

--- semafor_unittest.py
+++ semafor_unittest.py
@@ -4,7 +4,7 @@
 from deepnlpf.pipeline import Pipeline
 
 sentence = "I went to the bank to deposit my money."
-path_pipeline = "/home/fasr/Developer/deepnlpf/deepnlpf/tests/pipelines/json/semafor.json"
+path_pipeline = "/home/fasr/Mestrado/deepnlpf/tests/pipelines/json/semafor.json"
 
 nlp = Pipeline(_input=sentence, pipeline=path_pipeline, _output='file', boost='ray')
 annotation = nlp.annotate()
